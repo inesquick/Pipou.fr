7508a2b16560ffbd1a63d4c037e147c7ffcbec6b
inesquick <ineschtioui02@yahoo.fr>
2021-11-29 15:30:47 +0100

    try

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about-us', function(){
-    return view('about-us');
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
