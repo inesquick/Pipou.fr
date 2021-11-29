8e43862fc84830f8583a5d0e966cef719752482e
inesquick <ineschtioui02@yahoo.fr>
2021-11-29 15:00:13 +0100

    lol

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Steam</title>
+        <title>Pipou.fr</title>
 
 
     </head>
